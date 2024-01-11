9e3251285afffe753dadd98fa6bdc3006bce2614
Takumi-01 <76190751+Takumi-01@users.noreply.github.com>
2024-01-11 16:53:33 +0900

    修正

--- oldMaid.js
+++ oldMaid.js
@@ -201,28 +201,24 @@ class Card extends Obj{
     
     push();
     translate(this.pos.x, this.pos.y);
-    image(cardImg[0], 0, 0, this.size, 2 * this.size);
+    image(cardImg[randoms[0]], 0, 0, this.size, 2 * this.size);
     pop();
-    // push();
-    // translate(this.pos.x, this.pos.y);
-    // image(cardImg[randoms[0]], 0, 0, this.size, 2 / this.size);
-    // pop();
-    // push();
-    // translate(this.pos.x, this.pos.y);
-    // image(cardImg[randoms[1]], 10, 0, this.size, 2 / this.size);
-    // pop();
-    // push();
-    // translate(this.pos.x, this.pos.y);
-    // image(cardImg[randoms[2]], 20, 0, this.size, 2 / this.size);
-    // pop();
-    // push();
-    // translate(this.pos.x, this.pos.y);
-    // image(cardImg[randoms[3]], 30, 0, this.size, 2 / this.size);
-    // pop();
-    // push();
-    // translate(this.pos.x, this.pos.y);
-    // image(cardImg[randoms[4]], 40, 0, this.size, 2 / this.size);
-    // pop();
+    push();
+    translate(this.pos.x, this.pos.y);
+    image(cardImg[randoms[1]], 10, 0, this.size, 2 * this.size);
+    pop();
+    push();
+    translate(this.pos.x, this.pos.y);
+    image(cardImg[randoms[2]], 20, 0, this.size, 2 * this.size);
+    pop();
+    push();
+    translate(this.pos.x, this.pos.y);
+    image(cardImg[randoms[3]], 30, 0, this.size, 2 * this.size);
+    pop();
+    push();
+    translate(this.pos.x, this.pos.y);
+    image(cardImg[randoms[4]], 40, 0, this.size, 2 * this.size);
+    pop();
   }
   setTarget(target) {
     this.amt = 0;
