7f6d4ad459b0f64b13e4273b44e62ead94bde67d
Takumi-01 <76190751+Takumi-01@users.noreply.github.com>
2024-01-31 19:05:02 +0900

    修正

--- oldMaid.js
+++ oldMaid.js
@@ -487,7 +487,7 @@ function handCardDisplay(){
   for(num = 0; num < player.length; num++){
     push();
     translate(localVideo.x/3+((num - 2 / player.length) * 40), localVideo.y/3);
-    rotate(this.rotation);
+    rotate(0);
     image(cardImg[player[num]], 0, 0, this.size, 2 * this.size);
     pop();
   }
