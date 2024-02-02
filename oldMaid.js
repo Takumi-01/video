4e9c654a49dd24fc005fc652c4e56fee1214a0c8
Takumi-01 <76190751+Takumi-01@users.noreply.github.com>
2024-02-02 22:28:35 +0900

    修正

--- oldMaid.js
+++ oldMaid.js
@@ -439,7 +439,7 @@ class Card extends Obj{
     let num;
     for(num = 0; num < player.length; num++){
       push();
-      translate(widht / 2 + ((num - 2 / player.length) * 40), height / 3);
+      translate(width / 2 + ((num - 2 / player.length) * 40), height / 3);
       rotate(this.rotation);
       image(cardImg[player[num]], 0, 0, this.size, 2 * this.size);
       pop();
