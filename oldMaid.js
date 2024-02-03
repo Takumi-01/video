dc34781d311087158252f950cf530a3ae62320c9
Takumi-01 <76190751+Takumi-01@users.noreply.github.com>
2024-02-04 01:06:30 +0900

    修正

--- oldMaid.js
+++ oldMaid.js
@@ -490,9 +490,9 @@ class Card extends Obj{
       pop();
     }
     for(OLength = 0; OLength < cardManager.member.length; OLength++){
-      for(num = 0; num < opponent.length; num++){
+      for(num = 0; num < opponent[OLength].length; num++){
         push();
-        translate(others[OLength].x+((num - 2 / opponent.length) * 20), others[OLength].y);
+        translate(others[OLength].x+((num - 2 / opponent[OLength].length) * 20), others[OLength].y);
         rotate(radians(0));
         //rotate(others[OLength].rotation);
         image(tranpImg[0], 0, 0, this.size, 2 * this.size);
